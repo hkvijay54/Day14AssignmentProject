95dd78c9bdf57c956f931f2b8a91032bf3886486
hkvijay54 <hkvijay54@gmail.com>
2022-01-08 16:08:40 +0530

    samp

--- Day14AssignmentProject/Program.cs
+++ Day14AssignmentProject/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Datastructure programs");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day14AssignmentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            ListAddClass list = new ListAddClass();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("****Program End***\n");
+        }
+    }
+}
